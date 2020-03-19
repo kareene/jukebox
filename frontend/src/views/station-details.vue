477ac979fb0a6dd01e997da248778e2503759e91
Kareene Komet <kareene@kareene.com>
2020-03-19 14:37:10 +0200

    reder video and song list manipulation

--- frontend/src/views/station-details.vue
+++ frontend/src/views/station-details.vue
@@ -1,25 +1,23 @@
 <template>
-  <article class="station-details">
+  <article v-if="station" class="station-details">
     <h2>Station Details</h2>
-    <div v-if="station">
 
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/M-aoyPa41Ic" 
+    <div v-if="currSong" class="video-container">
+        <iframe width="560" height="315" :src="currSong.videoUrl" 
         frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
         </iframe>
     </div>
 
-      <h1>{{station.name}}</h1>
-      <h2>Create by: {{station.createdBy.fullName}}</h2>
-      <h4>{{station.tags.join(", ")}}</h4>
-      <h4>Likes: {{likedCount}}</h4>
-      <ul>
-        <li :key="song.id" v-for="song in station.songs">
-          <h3>{{song.title}}</h3>
-        </li>
-      </ul>
+    <h1>{{station.name}}</h1>
+    <h2>Create by: {{station.createdBy.fullName}}</h2>
+    <h4>{{station.tags.join(", ")}}</h4>
+    <h4>Likes: {{likedCount}}</h4>
+    <ul>
+      <li :key="song.id" v-for="song in station.songs">
+        <h3>{{song.title}}</h3>
+      </li>
+    </ul>
       
-
-    </div>
   </article>
 </template>
 
@@ -32,8 +30,9 @@ export default {
       currSong: null
     }
   },
-  created() {
-    this.loadStation();
+  async created() {
+    await this.loadStation();
+    this.currSong = this.station.songs.shift();
   },
   computed:{
     likedCount(){
@@ -44,7 +43,11 @@ export default {
     async loadStation() {
       const stationId = this.$route.params.id;
       const station = await this.$store.dispatch({ type: 'loadStation', stationId });
-      this.station = station;
+      this.station = JSON.parse(JSON.stringify(station));
+    },
+    playNextSong() {
+      this.station.songs.push(this.currSong);
+      this.currSong = this.station.songs.shift();
     }
   }
 }

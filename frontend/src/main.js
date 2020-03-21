d1dfbc2385d04b21e5e93f49b63f72df315515a5
limor ala <limorelv@gmail.com>
2020-03-21 15:56:05 +0200

    bla

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
-import './fonts/fontawesome/css/all.css'
+import './assets/fonts/fontawesome/css/all.css'
 import './styles/global.scss';
 
 Vue.config.productionTip = false

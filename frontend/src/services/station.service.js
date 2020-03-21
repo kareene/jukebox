e5548607f321767a1b439f77fd931d5319e5737e
limor ala <limorelv@gmail.com>
2020-03-21 19:04:24 +0200

    styling

--- frontend/src/services/station.service.js
+++ frontend/src/services/station.service.js
@@ -199,6 +199,59 @@ function _createStations() {
                     }
                 ],
                 theme: 'happy'
+            },
+            {
+                _id: 'lkjfgdiufgypyu2t',
+                name: 'Far away from home',
+                tags: ['dance', 'happy'],
+                imgUrl: 'https://images.8tracks.com/imgix/i/009/659/911/Z3B5CBI-8362.gif?rect=72,0,422,422&q=65&fit=max',
+                createdBy: {
+                    _id: 'eksnfits',
+                    fullName: 'Rotem Yehiel',
+                    urlImg: 'api.adorable.io/avatars/100/20.png'
+                },
+                likedBy: [{
+                        _id: 'edfjhfdg',
+                        fullName: 'Kareene Komet',
+                        urlImg: 'api.adorable.io/avatars/100/3.png'
+                    },
+                    {
+                        _id: 'edfjhfdg',
+                        fullName: 'Limor Abaev',
+                        urlImg: 'api.adorable.io/avatars/100/2.png'
+                    }
+                ],
+                songs: [{
+                        id: 'dfsgdfgsdf',
+                        title: 'Mahmood - Soldi',
+                        videoUrl: 'https://www.youtube.com/embed/M-aoyPa41Ic',
+                        addedBy: {
+                            _id: 'eksnfits',
+                            fullName: 'Rotem Yehiel',
+                            urlImg: 'api.adorable.io/avatars/100/20.png'
+                        }
+                    },
+                    {
+                        id: 'lkdfdsjfkgh',
+                        title: 'Verka Serduchka - Dancing Lasha Tumbai',
+                        videoUrl: 'https://www.youtube.com/embed/hfjHJneVonE',
+                        addedBy: {
+                            _id: 'eksnfits',
+                            fullName: 'Rotem Yehiel',
+                            urlImg: 'api.adorable.io/avatars/100/20.png'
+                        }
+                    }
+                ],
+                chatMsgs: [{
+                        fullName: 'Rotem Yehiel',
+                        txt: 'Welcome to the house of fun'
+                    },
+                    {
+                        fullName: 'Guest',
+                        txt: 'Graet music'
+                    }
+                ],
+                theme: 'happy'
             }
         ];
         localStorage.setItem('stations', JSON.stringify(stations));

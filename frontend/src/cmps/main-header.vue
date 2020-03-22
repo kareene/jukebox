199cff10f6cf6bc7125221d95cd5b2039da82a33
limor ala <limorelv@gmail.com>
2020-03-22 12:07:52 +0200

    hero style

--- frontend/src/cmps/main-header.vue
+++ frontend/src/cmps/main-header.vue
@@ -1,11 +1,14 @@
 <template>
-  <header class="main-header">
-    <h1>Jukebox</h1>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/station">Stations</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+  <header class="main-header flex direction-column">
+    <div class = "nav-container flex space-between">
+      <h1>Jukebox</h1>
+      <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/station">Stations</router-link> |
+        <router-link to="/about">About</router-link>
+      </nav>
+    </div>
+    <h2>Make and Discover Playlists</h2>
   </header>
 </template>
 

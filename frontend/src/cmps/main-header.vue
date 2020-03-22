4f57ffda399b03f73fcc0353d96d8ffabe992f75
limor ala <limorelv@gmail.com>
2020-03-22 20:04:13 +0200

    hero

--- frontend/src/cmps/main-header.vue
+++ frontend/src/cmps/main-header.vue
@@ -8,12 +8,39 @@
         <router-link to="/about">About</router-link>
       </nav>
     </div>
-    <h2>Make and Discover Playlists</h2>
+    <div class = "hero flex direction-column" :class = 'heroSize'>
+       <h2 >Make and Discover Playlists</h2>
+       <router-link to="/station/edit">Add New Station</router-link>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'mainHeader'
+  name: 'mainHeader',
+  data(){
+    return {
+      isHomePage: false
+    }
+  },
+  created(){
+    console.log("nav created" ,this.$route.name);
+    this.isHomePage = (this.$route.name =='homePage' || this.$route.name === 'stationsPage') ? true : false;
+  },
+
+  computed: {
+    heroSize(){
+      return (this.isHomePage) ? 'full' : 'small';
+    }
+  },
+  watch: {
+    '$route'(to, from) {
+            // console.log(to.name == 'homePage')
+            // console.log(this.isHomePage)
+            if(to.name === 'homePage' || to.name === 'stationsPage') this.isHomePage = true
+            else this.isHomePage = false
+            // console.log(this.isHomePage);
+        }
+  }
 }
 </script>
